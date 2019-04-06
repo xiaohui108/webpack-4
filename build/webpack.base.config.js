79861321351da9118dd73c10411883a9deb024e4
wanghuiping <wanghuiping@126.com>
2019-04-06 17:06:46 +0800

    修复bug

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -33,7 +33,7 @@ module.exports = {
       ...(config.dev.useEslint ? [webpackEslintRule()] : []),
       {
         test: /\.js$/,
-        // exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
